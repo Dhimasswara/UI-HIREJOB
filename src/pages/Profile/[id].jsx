f8465e5e487b67cf9cfa47009fe5dd68989ad78b
Dhimas Pandu Yogaswara <79622243+Dhimasswara@users.noreply.github.com>
2023-04-17 21:42:54 +0700

    modified profile error v2

--- src/pages/Profile/[id].jsx
+++ src/pages/Profile/[id].jsx
@@ -86,13 +86,13 @@ const Profile = () => {
                     <LeftSide>
                         <div className="profileDesc">
                             <div className={` mx-auto pb-4 pt-2 ${style.picture} d-flex justify-content-center`}>
-                                <Image crossOrigin='anonymouse' width={150} height={150} src={worker?.image === null ? '' : worker?.image} className={`img-thumbnail rounded-circle ${style.imgStyle}`}></Image>
+                                <Image crossOrigin='anonymouse' width={150} height={150} src={worker?.image === null ? img : worker?.image} className={`img-thumbnail rounded-circle ${style.imgStyle}`}></Image>
                             </div>
                             <h5 className='fw-bolder'>{worker?.name}</h5>
-                            <span className={style.work}>{worker?.jobdesk === null || 'null' ? 'Position' : worker?.jobdesk}</span>
+                            <span className={style.work}>{worker?.jobdesk === null || 'null' ? 'Jobdesk' : worker?.jobdesk}</span>
                             <p className={style.address}><FontAwesomeIcon icon={faLocationDot} style={{ marginRight: "10px" }} />{worker?.address === null || 'null' ? 'Location' : worker?.address}</p>
-                            <span className={style.worker}>{worker?.jobdesk === null || 'null' ? 'Position' : worker?.jobdesk}</span>
-                            <p className={style.bio}>{worker?.description === null || 'null' ? 'Position' : worker?.description}</p>
+                            <span className={style.worker}>{worker?.jobdesk === null || 'null' ? 'Jobdesk' : worker?.jobdesk}</span>
+                            <p className={style.bio}>{worker?.description === null || 'null' ? 'Description' : worker?.description}</p>
                             {role === 'recruiter' ?
                                 <div className="hireButton d-grid">
                                     <button className={`btn ${style.btn}`}> Hire</button>
