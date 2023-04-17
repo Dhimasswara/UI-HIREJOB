4c968a9c7ef94bc5aa304caef07c0368b9e7b978
Dhimas Pandu Yogaswara <dhimaspanduyoga@gmail.com>
2023-04-17 12:31:57 +0700

    modified pages

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
@@ -10,7 +9,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript/>
-        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossOrigin="anonymous"></Script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossOrigin="anonymous"></script>
       </body>
     </Html>
   );
